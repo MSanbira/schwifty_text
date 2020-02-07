99021e54a1eda150c0180385bce60ffccba99a35
LMSanbira <matan@loadmill.com>
2020-02-07 23:22:00 +0200

    added no text and btn

--- src/demo.js
+++ src/demo.js
@@ -16,39 +16,70 @@ export default function Demo() {
         text="An animated gradient filled text component"
         variant="h2"
         containerSize="l"
-        textStyle={{ textAlign: "center" }}
+        textStyle={{ textAlign: "center", padding: 0 }}
+        options={{ inverse: true }}
       />
 
       <div className="two-cols-container">
-        <SchwiftyText containerSize="m">
-          <img src={schwiftyRick} />
-        </SchwiftyText>
-        <SchwiftyText containerSize="m">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </p>
-          <p>
-            Elit scelerisque mauris pellentesque pulvinar pellentesque habitant
-            morbi tristique senectus. A cras semper auctor neque. At quis risus
-            sed vulputate. Erat pellentesque adipiscing commodo elit at
-            imperdiet. Leo urna molestie at elementum eu facilisis sed odio
-            morbi. Massa tincidunt nunc pulvinar sapien et ligula ullamcorper.
-            Dictumst vestibulum rhoncus est pellentesque elit. Sem nulla
-            pharetra diam sit amet nisl suscipit. Duis convallis convallis
-            tellus id interdum velit. Eget egestas purus viverra accumsan in.
-            Laoreet non curabitur gravida arcu ac tortor dignissim convallis.
-          </p>
-          <div>I'm not rendered</div>
-          <p>
+        <div style={{ width: "60%" }}>
+          <SchwiftyText containerSize="m">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <div>Wobba labba dub dub</div>
+          </SchwiftyText>
+          <SchwiftyText containerSize="m" options={{ speed: "fast" }}>
+            <p>
+              Elit scelerisque mauris pellentesque pulvinar pellentesque
+              habitant morbi tristique senectus. A cras semper auctor neque. At
+              quis risus sed vulputate. Erat pellentesque adipiscing commodo
+              elit at imperdiet. Leo urna molestie at elementum eu facilisis sed
+              odio morbi. Massa tincidunt nunc pulvinar sapien et ligula
+              ullamcorper. Dictumst vestibulum rhoncus est pellentesque elit.
+              Sem nulla pharetra diam sit amet nisl suscipit. Duis convallis
+              convallis tellus id interdum velit. Eget egestas purus viverra
+              accumsan in. Laoreet non curabitur gravida arcu ac tortor
+              dignissim convallis.
+            </p>
+          </SchwiftyText>
+          <SchwiftyText
+            variant="p"
+            containerSize="m"
+            options={{ speed: "superFast" }}
+          >
             Duis aute irure dolor in reprehenderit in voluptate velit esse
             cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
             cupidatat non proident, sunt in culpa qui officia deserunt mollit
             anim id est laborum.
-          </p>
+          </SchwiftyText>
+        </div>
+
+        <SchwiftyText containerSize="m">
+          <img src={schwiftyRick} alt="Get Schwifty!" />
         </SchwiftyText>
+      </div>
+
+      <div className="arrow-btn-container">
+        <SchwiftyText
+          options={{ noText: true, speed: "superFast" }}
+          className="arrow-right"
+          containerSize="s"
+        />
+        <SchwiftyText
+          options={{ spacialElement: "btn" }}
+          containerSize="s"
+          style={{ width: "100%" }}
+        >
+          press me
+        </SchwiftyText>
+        <SchwiftyText
+          options={{ noText: true, reverse: true, speed: "superFast" }}
+          className="arrow-left"
+          containerSize="s"
+        />
       </div>
     </div>
   );
--- src/schwifty-text.js
+++ src/schwifty-text.js
@@ -3,17 +3,26 @@ import "./schwifty-text.scss";
 
 const variantOptions = ["h1", "h2", "h3", "h4", "h5", "h6", "p", "img"];
 const containerSizeOptions = {
-  small: "s-width",
-  s: "s-width",
-  medium: "m-width",
-  m: "m-width",
-  large: "l-width",
-  l: "l-width",
-  extraLarge: "xl-width",
-  xl: "xl-width"
+  small: "SWIF-s-w",
+  s: "SWIF-s-w",
+  medium: "SWIF-m-w",
+  m: "SWIF-m-w",
+  large: "SWIF-l-w",
+  l: "SWIF-l-w",
+  extraLarge: "SWIF-xl-w",
+  xl: "SWIF-xl-w"
+};
+const containerSpeedOptions = {
+  superFast: "SWIF-s-fast",
+  fast: "SWIF-fast",
+  normal: "SWIF-normal",
+  slow: "SWIF-slow"
+};
+const spacialElements = {
+  btn: "SWIF-btn"
 };
 
-// TODO - children, inverse, img
+// TODO - btns, divider
 export default function SchwiftyText(props) {
   const {
     containerSize = "m",
@@ -23,21 +32,37 @@ export default function SchwiftyText(props) {
     className = "",
     style = {},
     children,
-    textStyle = {}
+    textStyle = {},
+    options = {}
   } = props;
 
-  const textElements = [];
-  const containerSizeClass = Object.keys(containerSizeOptions).includes(
-    containerSize
-  )
-    ? containerSizeOptions[containerSize]
-    : "m-width";
-
   const colorsStyle = {};
+  const optionStyle = {};
+  const optionTextStyle = {};
+  const classes = ["schwifty-text"];
+  const textElements = [];
+
+  classes.push(className);
+  classes.push(containerSizeOptions[containerSize] || "SWIF-m-w");
+
   if (!!colors && colors.length >= 2) {
     colorsStyle["--schwifty-fir-bg-color"] = colors[0];
     colorsStyle["--schwifty-sec-bg-color"] = colors[1];
     colorsStyle["--schwifty-thr-bg-color"] = colors[2] || colors[1];
+  }
+
+  if (options.inverse) {
+    optionTextStyle.background = "black";
+    optionTextStyle.color = "white";
+  }
+  if (options.speed) {
+    classes.push(containerSpeedOptions[options.speed] || "SWIF-normal");
+  }
+  if (options.reverse) {
+    classes.push("SWIF-reverse");
+  }
+  if (options.spacialElement) {
+    classes.push(spacialElements[options.spacialElement] || "");
   }
 
   if (children) {
@@ -45,20 +70,32 @@ export default function SchwiftyText(props) {
       for (const child of children) {
         textElements.push(getElementFromChild(child));
       }
+    } else if (typeof children === "string") {
+      const ElementType = variantOptions.includes(variant) ? variant : "h1";
+      textElements.push(
+        <ElementType style={{ ...textStyle, ...optionTextStyle }}>
+          {children}
+        </ElementType>
+      );
     } else {
       textElements.push(getElementFromChild(children));
     }
   } else {
     const ElementType = variantOptions.includes(variant) ? variant : "h1";
-    textElements.push(<ElementType style={textStyle}>{text}</ElementType>);
+    textElements.push(
+      <ElementType style={{ ...textStyle, ...optionTextStyle }}>
+        {text}
+      </ElementType>
+    );
   }
 
   return (
     <div
-      className={`schwifty-text ${containerSizeClass} ${className}`}
-      style={{ ...style, ...colorsStyle }}
+      className={classes.join(" ")}
+      style={{ ...style, ...colorsStyle, ...optionStyle }}
     >
-      {textElements}
+      {!options.noText &&
+        (textElements.length === 1 ? textElements[0] : textElements)}
     </div>
   );
 }
